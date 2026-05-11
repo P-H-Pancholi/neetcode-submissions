81e604de4df163188cfee8dd4952286425cb9623
Phalin Pancholi <pancholiphalin@outlook.com>
2026-05-11 22:13:33 +0530

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max_sum = INT_MIN;
+        int curr_sum = 0;
+        for(int i : nums){
+            curr_sum += i;
+            max_sum = max(curr_sum, max_sum);
+            if(curr_sum < 0) curr_sum = 0;
+        }
+        return max_sum;
+    }
+};
