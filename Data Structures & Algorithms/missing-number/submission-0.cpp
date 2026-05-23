dd216665974e334d0d705b9883deef2c9c833529
Phalin Pancholi <pancholiphalin@outlook.com>
2026-05-23 14:42:59 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int res = 0;
+        for(int i = 1; i <= n; i++){
+            res = res ^ i;
+        }
+        for(int i : nums){
+            res = res ^ i;
+        }
+        return res;
+    }
+};
